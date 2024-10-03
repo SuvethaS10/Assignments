4495e9cd887e1e3861cfec98e79a7d34e868ae01
SuvethaS10 <suvethas10@gmail.com>
2024-10-03 17:14:23 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer ="Google";
+		boolean isBetaVersion = false;
+		int releaseYear = 2008;
+		char shortcut = 'c';
+		
+		System.out.println("The detail of browser:");
+		System.out.println("Name of browser:"+developer+"  "+"Version is:" +version+"  "+"beta type:"  +isBetaVersion+ "  "+"Release year:" +releaseYear+ "  "+"shortcut is:" +shortcut);	
+	}				
+}
