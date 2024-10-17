7d8f2b88a98dd9cfdfc27463baa480dc5e27b5c4
SuvethaS10 <suvethas10@gmail.com>
2024-10-17 10:59:00 +0530

    Add files via upload

---
+++ DataType_Varible.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class DataType_Varible
+{
+	
+	public static void main(String[] args) 
+	{
+		int i=10;
+		System.out.println("The value of int i is:"+i);
+		int k=30, j=20;
+		System.out.println("The vale of int k is:"+k +" "+ "The vaule of int j is:"+j);
+		float a=0.56732456098f;
+		System.out.println("The value of float a is:"+a);
+		double ab=0.12345678999990;
+		System.out.println("The value of double ab is:"+ab);
+		long q=12345678900l;
+		System.out.println("The value of long q is:"+q);
+		char intial='s';
+		System.out.println("The intial is:" +intial);
+		byte b=3;
+		System.out.println("The byte value is:" +b);
+		
+	}
+
+}
